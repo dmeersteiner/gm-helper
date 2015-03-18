ee864f6c318b926e7e05c285a901681e4f00411b
David Meersteiner <dmeersteiner@gmail.com>
2015-03-18 16:13:26 +0100

    after merge stuff

--- GM Helper/src/de/dm/gmhelper/gui/notes/PnlFastNotesPane.java
+++ GM Helper/src/de/dm/gmhelper/gui/notes/PnlFastNotesPane.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.List;
 
 import javax.swing.border.EmptyBorder;
 
